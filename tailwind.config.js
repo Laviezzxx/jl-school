be92b8f686aecd72eba658ccfa980394a32928f0
Zeon3Tim1 <juntian_2026@163.com>
2025-12-17 16:12:25 +0800

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], 
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',   /* 务实蓝 */
+        secondary: '#f97316', /* 活力橙 */
+      }
+    }
+  },
+  plugins: [],
+}
